774903b5e94925390ac6f39903cd09491ceefb43
Nikola Seomkin <seomkin.n@yandex.ru>
2026-07-24 20:47:07 +0300

    test

--- roadmap/05-db/5.1-sql-postgres/tasks.sql
+++ roadmap/05-db/5.1-sql-postgres/tasks.sql
@@ -1,3 +1,4 @@
+
 --В этом файле я буду решать задачи sql.
 -- Буду брать задачи и решать их из разных сайтов, ИИ и т.п.
 
@@ -235,3 +236,53 @@ ORDER BY created_at;
 Напиши индекс, который ускорит этот запрос. Объясни, почему ты выбрал именно такой индекс, и как проверить, что он работает.
  */
  CREATE INDEX idx_orders_created_status ON orders (created_at,status)
+
+
+/*
+Топ-3 товара по категориям
+Условие:
+Есть таблицы:
+
+products (product_id, name, category_id)
+
+categories (category_id, category_name)
+
+order_items (order_id, product_id, quantity, price, order_date)
+
+Нужно для каждой категории вывести три товара с самой большой общей выручкой
+ (quantity * price) за последний квартал (3 месяца от текущей даты). 
+ Если в категории меньше 3 товаров — вывести все.
+
+Результат:
+category_name, product_name, total_revenue, rank_in_category
+*/
+
+WITH product_revenue AS(
+    SELECT
+        p.product_id,
+        p.name AS product_name,
+        p.category_id,
+        c.category_name,
+        SUM(oi.quantity * io.price) AS total_revenue
+    FROM products p
+    JOIN order_items oi ON p.product_id = oi.product_id
+    JOIN categories c ON p.category_id = c.category_id    
+    WHERE io.order_date >= NOW() - INTERVAL '3 mounths'
+    GROUP BY p.product_id, p.name, p.category_id, c.category_name
+),
+ranked AS(
+    SELECT
+        category_name,
+        product_name,
+        total_revenue,
+        ROW_NUMBER() OVER(PARTITION BY category_id ORDER BY total_revenue DESC) AS rank_in_category
+        FROM product_revenue
+)
+SELECT
+    category_name,
+    product_name,
+    total_revenue,
+    rank_in_category
+FROM ranked
+WHERE rank_in_category <= 3
+ORDER BY category_name, rank_in_category;
